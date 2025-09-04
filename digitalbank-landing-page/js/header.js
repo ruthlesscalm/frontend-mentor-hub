21f206b931175ed836861460d4b157f6ea1cb57c
Ruthless Calm <ruthlesscalm@proton.me>
2025-09-04 17:55:43 +0530

    feat: implement responsive navigation menu for mobile devices

--- digitalbank-landing-page/js/header.js
+++ digitalbank-landing-page/js/header.js
@@ -1,6 +1,8 @@
 const menuBtn = document.getElementById("menu-button");
 const menuIcon = document.getElementById("menu-icon");
+const navBox = document.getElementById("nav-box");
 
-menuBtn.addEventListener("click", ()=>{
+menuBtn.addEventListener("click", () => {
     menuIcon.classList.toggle("header__menu-icon--cross");
+    navBox.style.display = navBox.style.display == "block" ? "none" : "block";
 })
