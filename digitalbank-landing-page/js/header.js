689140b4313e1cf586d3c2f477cdc6d1d5d11d17
Ruthless Calm <ruthlesscalm@proton.me>
2025-09-10 21:34:53 +0530

    feat: enhance header styles and improve navigation functionality

--- digitalbank-landing-page/js/header.js
+++ digitalbank-landing-page/js/header.js
@@ -2,7 +2,9 @@ const menuBtn = document.getElementById("menu-button");
 const menuIcon = document.getElementById("menu-icon");
 const navBox = document.getElementById("nav-box");
 
-menuBtn.addEventListener("click", () => {
+menuBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
     menuIcon.classList.toggle("header__menu-icon--cross");
+    document.getElementsByTagName("main")[0].classList.toggle("overlay");
     navBox.style.display = navBox.style.display == "block" ? "none" : "block";
 })
